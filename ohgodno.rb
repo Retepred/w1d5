a015c15f4e6d0ff3460c97571ab7fa1cc6d02576
Retepred <peter.c.t.hyder@googlemail.com>
2015-10-10 15:21:54 +0100

    whywontyouwork

--- ohgodno.rb
+++ ohgodno.rb
@@ -1,7 +1,9 @@
 require "pry-byebug"
 puts `clear`
 
-@product_list = {"a1" => "needle", "a2" => "stop sign", "a3" => "blouse", "a4" => "hanger", "a5" => "rubber duck", "a6" => "shovel", "a7" => "bookark", "a8" => "model car", "a9" => "glow stick", "a10" => "rubber band", "b1" => "tire swing", "b2" => "sharpie", "b3" => "picture frame", "b4" => "photo album", "b5" => "nail filer", "b6" => "tooth paste", "b7" => "bath fizzers", "b8" => "tissue box", "b9" => "deoderant", "b10" => "cookie jar", "c1" => "rusty nail", "c2" => "drill press", "c3" => "chalk", "c4" => "word search", "c5" => "thermometer", "c6" => "face wash", "c7" => "paint brush", "c8" => "candy wrapper", "c9" => "shoe lace", "c10" => "leg warmers" }
+@product_list = {"a1" => "needle", "a2" => "stop sign", "a3" => "blouse", "a4" => "hanger", "a5" => "rubber duck", "a6" => "shovel", "a7" => "bookmark", "a8" => "model car", "a9" => "glow stick", "a10" => "rubber band", "b1" => "tire swing", "b2" => "sharpie", "b3" => "picture frame", "b4" => "photo album", "b5" => "nail filer", "b6" => "tooth paste", "b7" => "bath fizzers", "b8" => "tissue box", "b9" => "deoderant", "b10" => "cookie jar", "c1" => "rusty nail", "c2" => "drill press", "c3" => "chalk", "c4" => "word search", "c5" => "thermometer", "c6" => "face wash", "c7" => "paint brush", "c8" => "candy wrapper", "c9" => "shoe lace", "c10" => "leg warmers" }
+
+@product_list_2 = {"needle" => "a1", "stop sign" => "a2", "blouse" => "a3", "hanger" => "a4", "rubber duck" => "a5", "shovel" => "a6", "bookmark" => "a7", "model car" => "a8", "glow stick" => "a9", "rubber band" => "a10", "tire swing" => "b1", "sharpie" => "b2", "picture frame" => "b3", "photo album" => "b4", "nail filer" => "b5", "tooth paste" => "b6", "bath fizzers" => "b7", "tissue box" => "b8", "deoderant" => "b9", "cookie jar" => "b10", "rusty nail" => "c1", "drill press" => "c2", "chalk" => "c3", "word search" => "c4", "thermometer" => "c5", "face wash" => "c6", "paint brush" => "c7", "candy wrapper" => "c8", "shoe lace"  => "c9", "leg warmers" => "c10" }
 
 @locations = ["a10", "a9", "a8", "a7", "a6", "a5", "a4", "a3", "a2", "a1", "c1", "c2", "c3", "c4", "c5", "c6", "c7", "c8", "c9", "c10", "b1", "b2", "b3", "b4", "b5", "b6", "b7", "b8", "b9", "b10"]
 
@@ -35,7 +37,7 @@ def item_locater
 end
 
 def exit
-
+puts `clear`
 end
   
 def distance_calculator
@@ -60,8 +62,12 @@ def collection_planner
   user_continue
   puts `clear`
   puts 'Please input the items you wish to find as shown "candy wrapper", "deoderant", "hanger"'
-  human_values = gets.chomp
-  puts @product_list.key(human_values)
+  human_values_original = gets.chomp
+
+
+  human_values = human_values_original
+  final_2 = @product_list_2.values_at(human_values)
+  puts final_2
   user_continue
   menu
 end  
